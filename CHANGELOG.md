ebde78a41d054fccff4deea10ec30f5a7087cbed
damir <damir1petkovic@gmail.com>
2024-10-01 20:22:14 +0200

    support passing name atom list as a module attribute

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.2.0 (2024-10-01)
+
+- Accept module attribute for the assign name atom list
+
 ## v0.1.0 (2024-09-28)
 
 - Initial implementation
--- lib/assigns.ex
+++ lib/assigns.ex
@@ -74,33 +74,118 @@ defmodule Assigns do
   end
   ```
   """
+  defmacro __using__( _env) do
+    Module.register_attribute( __CALLER__.module, :_keys, accumulate: true)
+
+    quote do
+      import Assigns,
+        only: [ defassign: 1, defassignp: 1, defassign_new: 1, defassign_newp: 1, defupdate: 1, defupdatep: 1]
+
+      @before_compile Assigns
+    end
+  end
+
+  # At this stage, just push the name(s) into an accumulating module
+  # attribute.  We'll generate the actual code in the @before_compile hook
+  # because it can resolve module attributes
 
   @doc  """
   Defines a `Phoenix.Component.assign/3` wrapper function for each of the names
   whether provided in a list or as a standalone atom.
   """
-  @spec defassign( atom() | [ atom()]) :: Macro.output()
+  @spec defassign( atom() | [ atom()]) :: :ok
   defmacro defassign( name_or_names) do
-    quote do
-      def_assign( :def, unquote( name_or_names))
-    end
+    Module.put_attribute( __CALLER__.module, :_keys, { :assign, :def, name_or_names})
   end
 
   @doc """
   Same as `defassign/1` but defines private wrapper function(s).
   """
-  @spec defassignp( atom() | [ atom()]) :: Macro.output()
+  @spec defassignp( atom() | [ atom()]) :: :ok
   defmacro defassignp( name_or_names) do
-    quote do
-      def_assign( :defp, unquote( name_or_names))
-    end
+    Module.put_attribute( __CALLER__.module, :_keys, { :assign, :defp, name_or_names})
   end
 
-  @doc false
-  @spec def_assign( :def | :defp, atom() | [ atom()]) :: Macro.output()
-  defmacro def_assign( kind, name_or_names)
+  @doc """
+  Defines a `Phoenix.Component.assign_new/3` wrapper function for each of the
+  names whether provided in a list or as a standalone atom.
+  """
+  @spec defassign_new( atom() | [ atom()]) :: Macro.output()
+  defmacro defassign_new( name_or_names) do
+    Module.put_attribute( __CALLER__.module, :_keys, { :assign_new, :def, name_or_names})
+  end
 
-  defmacro def_assign( kind, name) when kind in [ :def, :defp] and is_atom( name) do
+  @doc """
+  Same as `defassign_new/1` but defines private wrapper function(s).
+  """
+  @spec defassign_newp( atom() | [ atom()]) :: Macro.output()
+  defmacro defassign_newp( name_or_names) do
+    Module.put_attribute( __CALLER__.module, :_keys, { :assign_new, :defp, name_or_names})
+  end
+
+  @doc """
+  Defines a `Phoenix.Component.update/3` wrapper function for each of the names
+  whether provided in a list or as a standalone atom.
+  """
+  @spec defupdate( atom() | [ atom()]) :: Macro.output()
+  defmacro defupdate( name_or_names) do
+    Module.put_attribute( __CALLER__.module, :_keys, { :update, :def, name_or_names})
+  end
+
+  @doc """
+  Same as `defupdate/1` but defines private  wrapper function(s).
+  """
+  @spec defupdatep( atom() | [ atom()]) :: Macro.output()
+  defmacro defupdatep( name_or_names) do
+    Module.put_attribute( __CALLER__.module, :_keys, { :update, :defp, name_or_names})
+  end
+
+  # Traverses collected `_keys` attributes and generates code for each.
+  @doc false
+  defmacro __before_compile__( _env) do
+    __CALLER__.module
+    |> Module.delete_attribute( :_keys)
+    |> Assigns.resolve_and_flatten_keys( __CALLER__.module)
+    |> Enum.reverse()
+    |> Enum.into( [], &Assigns.expand_code/1)
+  end
+
+  @typep use_case() :: :assign | :assign_new | :update
+  @typep kind() :: :def | :defp
+  @typep key() :: { use_case(), kind(), atom()}
+
+  @doc false
+  @spec resolve_and_flatten_keys( list(), module()) :: [ key()]
+  def resolve_and_flatten_keys( keys, caller) do
+    Enum.map( keys, fn
+      { use_case, kind, { :@, _, [ { module_attribute, _, _}]}} ->
+        key_or_keys = Module.get_attribute( caller, module_attribute)
+        expand_keys( use_case, kind, key_or_keys)
+
+      { use_case, kind, key_or_keys} ->
+        expand_keys( use_case, kind, key_or_keys)
+    end)
+    |> List.flatten()
+  end
+
+  @spec expand_keys( use_case(), kind(), atom() | [ atom()]) :: key() | [ key()]
+  defp expand_keys( use_case, kind, name_or_names)
+
+  defp expand_keys( use_case, kind, names) when is_list( names) do
+    Enum.map( names, &{ use_case, kind, &1})
+    |> Enum.reverse()
+  end
+
+  defp expand_keys( use_case, kind, name) when is_atom( name) do
+    { use_case, kind, name}
+  end
+
+  # Generates function definitions
+  @doc false
+  @spec expand_code( key()) :: Macro.output()
+  def expand_code( key)
+
+  def expand_code( { :assign, kind, name}) do
     sanitized_name = Assigns.sanitize_name( name)
     var = Macro.var( name, nil)
 
@@ -111,40 +196,7 @@ defmodule Assigns do
     end
   end
 
-  defmacro def_assign( kind, [ first | _] = names) when kind in [ :def, :defp] and is_atom( first) do
-    for name <- names do
-      quote do
-        def_assign( unquote( kind), unquote( name))
-      end
-    end
-  end
-
-  @doc """
-  Defines a `Phoenix.Component.assign_new/3` wrapper function for each of the
-  names whether provided in a list or as a standalone atom.
-  """
-  @spec defassign_new( atom() | [ atom()]) :: Macro.output()
-  defmacro defassign_new( name_or_names) do
-    quote do
-      def_assign_new( :def, unquote( name_or_names))
-    end
-  end
-
-  @doc """
-  Same as `defassign_new/1` but defines private wrapper function(s).
-  """
-  @spec defassign_newp( atom() | [ atom()]) :: Macro.output()
-  defmacro defassign_newp( name_or_names) do
-    quote do
-      def_assign_new( :defp, unquote( name_or_names))
-    end
-  end
-
-  @doc false
-  @spec def_assign_new( :def | :defp, atom() | [ atom()]) :: Macro.output()
-  defmacro def_assign_new( kind, name_or_names)
-
-  defmacro def_assign_new( kind, name) when kind in [ :def, :defp] and is_atom( name) do
+  def expand_code( { :assign_new, kind, name}) do
     sanitized_name = Assigns.sanitize_name( name)
 
     quote do
@@ -154,40 +206,7 @@ defmodule Assigns do
     end
   end
 
-  defmacro def_assign_new( kind, [ first | _] = names) when is_atom( first) do
-    for name <- names do
-      quote do
-        def_assign_new( unquote( kind), unquote( name))
-      end
-    end
-  end
-
-  @doc """
-  Defines a `Phoenix.Component.update/3` wrapper function for each of the names
-  whether provided in a list or as a standalone atom.
-  """
-  @spec defupdate( atom() | [ atom()]) :: Macro.output()
-  defmacro defupdate( name_or_names) do
-    quote do
-      def_update( :def, unquote( name_or_names))
-    end
-  end
-
-  @doc """
-  Same as `defupdate/1` but defines private  wrapper function(s).
-  """
-  @spec defupdatep( atom() | [ atom()]) :: Macro.output()
-  defmacro defupdatep( name_or_names) do
-    quote do
-      def_update( :defp, unquote( name_or_names))
-    end
-  end
-
-  @doc false
-  @spec def_update( :def | :defp, atom() | [ atom()]) :: Macro.output()
-  defmacro def_update( kind, name_or_names)
-
-  defmacro def_update( kind, name) when kind in [ :def, :defp] and is_atom( name) do
+  def expand_code( { :update, kind, name}) do
     sanitized_name = Assigns.sanitize_name( name)
 
     quote do
@@ -197,14 +216,7 @@ defmodule Assigns do
     end
   end
 
-  defmacro def_update( kind, [ first | _] = names) when is_atom( first) do
-    for name <- names do
-      quote do
-        def_update( unquote( kind), unquote( name))
-      end
-    end
-  end
-
+  # Trims a trailing `?` from the name atom if any.
   @doc false
   @spec sanitize_name( atom()) :: atom()
   def sanitize_name( name) when is_atom( name) do
